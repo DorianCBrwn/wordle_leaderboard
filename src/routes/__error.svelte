0ff260a73f43072a00d8767d11424a7b3f9aba47
Dorian C Brown <brown.3794@gmail.com>
2022-07-20 13:00:16 -0400

    feat: :sparkles: add 404 error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = ({ error, status }) => {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="hero min-h-screen" style="background-image: url(./404.svg);">
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">{title}</h1>
+			<a href="/" class="btn btn-primary">Return Home</a>
+		</div>
+	</div>
+</div>
+<!--
+<style>
+	img {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		object-fit: cover;
+	}
+</style> -->
