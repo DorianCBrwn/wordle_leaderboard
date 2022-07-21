3af83afb9894ef81e37909ad349c545b90761bc5
Dorian C Brown <brown.3794@gmail.com>
2022-07-21 15:38:34 -0400

    feat: :sparkles: add leaderboard route

---
+++ src/lib/components/board.svelte
@@ -0,0 +1,14 @@
+<script>
+	const handleClick = (e) => {
+		console.log('clicked ' + e.target.id);
+	};
+</script>
+
+<section class="flex flex-col ">
+	<h1 class="text-center">LeaderBoard</h1>
+	<div class="btn-group justify-center">
+		<button class="btn" on:click={handleClick}>Daily</button>
+		<button class="btn" on:click={handleClick}>Monthly</button>
+		<button class="btn" on:click={handleClick}>All Time</button>
+	</div>
+</section>
